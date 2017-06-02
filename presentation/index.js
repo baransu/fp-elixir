c454177f859402f2dceb836323eb2b0f01cf318b
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-06-02 20:01:17 +0200

    Clean up assets

--- presentation/index.js
+++ presentation/index.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 import { Deck } from 'spectacle';
 
-// Import image preloader util
-import preloader from 'spectacle/lib/utils/preloader';
-
 // Import theme
 import createTheme from 'spectacle/lib/themes/default';
 
@@ -14,15 +11,6 @@ import 'spectacle/lib/themes/default/index.css';
 
 import * as s from './slides';
 const slides = Object.keys(s).reduce((acc, key) => acc.concat(s[key]), []);
-
-const images = {
-  city: require('../assets/city.jpg'),
-  kat: require('../assets/kat.png'),
-  logo: require('../assets/formidable-logo.svg'),
-  markdown: require('../assets/markdown.png')
-};
-
-preloader(images);
 
 const theme = createTheme(
   {
